4e8f04fed22915844a4b2dc0c1f1a69f1d089dd2
jamcplusplus <jamcplusplus@gmail.com>
2017-11-14 16:44:10 +0800

    add test for diy dictionary

--- tests/test_bayes.py
+++ tests/test_bayes.py
@@ -42,17 +42,19 @@ class BayesTest(unittest.TestCase):
             wrong_results = [
                 i for i, j in
                 zip(instance.test_classify, classify_results) if i != j]
-            return len(wrong_results)/len(instance.test_data)
+            return len(wrong_results) / len(instance.test_data)
 
         a = []
         test_times = 20
         print('\nThis may takes some time')
         for i in range(test_times):
             if i % 5 == 0:
-                print('Completed %s tasks, %s tasks left.' % (i, test_times-i))
+                print('Completed %s tasks, %s tasks left.' %
+                      (i, test_times - i))
             test_spam_filter = spam_filter.Filter(cache=False)
             a.append(error_rate(test_spam_filter))
-        print('The error rate is %s' % "{0:.2f}".format(sum(a)/test_times*100)+'%')
+        print('The error rate is %s' %
+              "{0:.2f}".format(sum(a) / test_times * 100) + '%')
 
     def test_data_num_correct(self):
         test_bayes = spam_filter.Filter(test_num=80, cache=False)
@@ -66,3 +68,40 @@ class BayesTest(unittest.TestCase):
         self.assertTrue(
             os.path.isfile(
                 os.path.join(spam_filter.BASE_DIR, 'cache/vocab.cache')))
+
+    def test_diy_dictionary(self):
+        '''
+        test error rate
+        '''
+        def error_rate(instance):
+            classify_results = []
+            for i in range(len(instance.test_data)):
+                test_result, percentage_list = (
+                    instance.bayes_classify(instance.test_data[i]))
+                classify_results.append(test_result)
+                # Uncomment  to see which sentence was classified wrong.
+                # if test_result != self.test_classify[i]:
+                #     print(self.test_data[i])
+                #     print('test_result is %s' % test_result)
+                #     print('true is %s' % self.test_classify[i])
+                #     print('percentage_list is %s' % percentage_list)
+            wrong_results = [
+                i for i, j in
+                zip(instance.test_classify, classify_results) if i != j]
+            return len(wrong_results) / len(instance.test_data)
+        a = []
+        test_times = 20
+        print('\nThis may takes some time')
+        for i in range(test_times):
+            if i % 5 == 0:
+                print('Completed %s tasks, %s tasks left.' %
+                      (i, test_times - i))
+            test_spam_filter = spam_filter.Filter(
+                cache=False, dictionary='dict.txt')
+            a.append(error_rate(test_spam_filter))
+        print('The error rate is %s' %
+              "{0:.2f}".format(sum(a) / test_times * 100) + '%')
+
+
+if __name__ == '__main__':
+    unittest.main()
