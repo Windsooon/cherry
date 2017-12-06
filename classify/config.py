d831399f771aebee6af5d1b1085c559a1030d5c5
Windson <wiwindson@outlook.com>
2017-12-06 14:03:46 +0800

    add different languages

--- classify/config.py
+++ classify/config.py
@@ -1,4 +1,20 @@
-NORMAL = 0
-GAMBLE = 1
-SEX = 2
-POLITICS = 3
+TYPE = {'NORMAL': 0,
+        'GAMBLE': 1,
+        'SEX': 2,
+        'POLITICS': 3}
+
+LAN = {'Chinese': 
+          {'type':
+              ['normal.dat',
+              'gamble.dat',
+              'sex.dat',
+              'politics.dat'],
+           'split': "nonspace",
+          },
+       'Englisth':
+           {'type':
+               ['normal.dat',
+               'spam.dat'],
+            'split': "nonspace",
+            },
+      }
