5b86cfddaaee2b03689d34b7ff9c02b6e411c19a
Windsooon <wiwindson@outlook.com>
2018-02-01 13:00:27 +0800

    clean code and update return value

--- classify/bayes.py
+++ classify/bayes.py
@@ -21,12 +21,11 @@ class Classify:
             cache=True,
             ):
 
-        self.vector_cache = os.path.join(BASE_DIR, 'cache/' + lan + '/vector.cache')
-        self.vocab_cache = os.path.join(BASE_DIR, 'cache/' + lan + '/vocab.cache')
+        self._set_path(lan)
+
         if cache:
             self._load_cache()
         else:
-            self.file_path = self._get_default_path_dir(lan)
             self.files_num = len(self.file_path)
             self._split_data(test_num)
             # topN wouldn't work with cache
@@ -38,25 +37,37 @@ class Classify:
             self.vector = self._get_vector()
             # Write self.vacab_list and self.vector as cache to file
             self._write_cache()
+    
+    def _set_path(self, lan):
+        all_data_path = os.path.join(BASE_DIR, ('all_data/' + lan + '/'))
+        self.vocab_cache_path = os.path.join(all_data_path, 'cache/vocab.cache')
+        self.vector_cache_path = os.path.join(all_data_path, 'cache/vector.cache')
+        self.classify_cache_path = os.path.join(all_data_path, 'cache/classify.cache')
+        self.stop_word_path = os.path.join(all_data_path, 'stop/stop_word.dat')
+        data_path = os.path.join(all_data_path, 'data/')
+        self.file_path = [data_path + x for x in os.listdir(data_path)]
+
+    def _get_stop_word_path(self, lan):
+        return os.path.join(BASE_DIR, 'data/' + lan + '/stop_word.dat')
 
     def _load_cache(self):
         try:
-            with open(self.vocab_cache, 'rb') as f:
+            with open(self.vocab_cache_path, 'rb') as f:
                 self.vocab_list = pickle.load(f)
-            with open(self.vector_cache, 'rb') as f:
+            with open(self.vector_cache_path, 'rb') as f:
                 self.vector = pickle.load(f)
-        except IOError:
+            with open(self.classify_cache_path, 'rb') as f:
+                self.CLASSIFY = pickle.load(f)
+        except IndexError:
                 raise IOError("Can't find cache files")
 
     def _write_cache(self):
-        with open(self.vocab_cache, 'wb') as f:
+        with open(self.vocab_cache_path, 'wb') as f:
             pickle.dump(self.vocab_list, f)
-        with open(self.vector_cache, 'wb') as f:
+        with open(self.vector_cache_path, 'wb') as f:
             pickle.dump(self.vector, f)
-    
-    def _get_default_path_dir(self, lan):
-        data_dir = os.path.join(BASE_DIR, 'data/' + lan + '/large/')
-        return [data_dir + x for x in os.listdir(data_dir)]
+        with open(self.classify_cache_path, 'wb') as f:
+            pickle.dump(self.CLASSIFY, f)
 
     def _read_files(self):
         '''
@@ -68,7 +79,7 @@ class Classify:
             [
                 "What a lovely day",
                 "Free porn videos and sex movie",
-                "I like to gamble",
+                "I like gambling",
                 "I love my dog sunkist"
             ]
 
@@ -78,6 +89,7 @@ class Classify:
         '''
         self.data_list = []
         self.classify = []
+        self.CLASSIFY = []
         for i in range(self.files_num):
             with open(self.file_path[i], encoding='utf-8') as f:
                 for k in f.readlines():
@@ -85,7 +97,13 @@ class Classify:
                     self.data_list.append(k)
                     # Store this sentence belongs to which category
                     self.classify.append(i)
+                # Get classify name
+                self.CLASSIFY.append(
+                    os.path.basename(os.path.normpath(self.file_path[i])))
         self.data_len = len(self.classify)
+
+        with open(self.stop_word_path, encoding='utf-8') as f:
+            self.stop_word_lst = [l[:-1] for l in f.readlines()]
 
     def _split_data(self, test_num):
         '''
@@ -104,7 +122,7 @@ class Classify:
 
         self.train_data:
             [
-                "I like to gamble",
+                "I like gambling",
                 "I love my dog sunkist"
             ]
 
@@ -116,7 +134,7 @@ class Classify:
         random_list = random.sample(range(0, self.data_len), test_num)
         # get test data
         self.test_data = [self.data_list[r] for r in random_list]
-        self.test_classify = [self.classify[r] for r in random_list]
+        self.test_classify = [self.CLASSIFY[self.classify[r]] for r in random_list]
         # get train data
         self.train_data = [
             self.data_list[r] for r in
@@ -148,7 +166,10 @@ class Classify:
                 else:
                     dic[i] = 1
         d = collections.Counter(dic)
-        vocab_lst = [i[0] for i in d.most_common() if len(i[0]) > 1]
+        vocab_lst = [
+            i[0] for i in d.most_common() if (len(i[0]) > 1
+            and i[0] not in self.stop_word_lst)
+        ]
         self.vocab_list = vocab_lst[n:]
 
     def _vocab_list(self):
@@ -168,7 +189,10 @@ class Classify:
         vocab_set = set()
         for k in self.train_data:
             vocab_set = vocab_set | set(jieba.cut(k))
-            self.vocab_list = [i for i in vocab_set if len(i) > 1]
+            self.vocab_list = [
+                i for i in vocab_set if (len(i) > 1
+                and i not in self.stop_word_lst)
+            ]
 
     def _sentence_to_vector(self, sentence):
         '''
@@ -212,7 +236,16 @@ class Classify:
         '''
         Compute softmax values for each sets of scores in x.
         '''
-        return np.exp(lst) / float(sum(np.exp(lst)))
+        return np.exp(lst) / np.sum(np.exp(lst), axis=0)
+
+    def _min_max(self, lst):
+        '''
+        Min-Max Normalization
+        '''
+        return [(x-min(lst))/(max(lst)-min(lst)) for x in lst]
+
+    def _clean_percentage_list(self, lst):
+        return list(zip(self.CLASSIFY, self._softmax(self._min_max(lst))))
 
     def bayes_classify(self, sentence):
         '''
@@ -220,9 +253,26 @@ class Classify:
         type: strings: strings
         '''
         word_vector = self._sentence_to_vector(sentence)
-        percentage_list = [
-            sum(i[0] * word_vector) + np.log(i[1]) for i in self.vector]
+        possibility_vector = []
+        percentage_list = []
+        for i in self.vector:
+            # final_vector: [0, -7.3, 0, 0, -8, ...]
+            final_vector = i[0] * word_vector
+            # word_index: [1, 4]
+            word_index = np.nonzero(final_vector)
+            # non_zero_word: [明天，永远]
+            non_zero_word = np.array(self.vocab_list)[word_index]
+            # non_zero_vector: [-7.3, -8]
+            non_zero_vector = final_vector[word_index]
+            possibility_vector.append(non_zero_vector)
+            percentage_list.append(sum(final_vector) + np.log(i[1]))
+        possibility_array = np.array(possibility_vector)
         max_val = max(percentage_list)
         for i, j in enumerate(percentage_list):
             if j == max_val:
-                return i, self._softmax(percentage_list)
+                max_array = possibility_array[i, :]
+                left_array = np.delete(possibility_array, i, 0)
+                sub_array = np.zeros(max_array.shape)
+                for k in left_array:
+                    sub_array += max_array - k
+                return self._clean_percentage_list(percentage_list), list(zip(non_zero_word, sub_array))
