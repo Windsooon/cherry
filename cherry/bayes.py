c83be0cd2b6ed8368942e1857cff33ee0d87ce9d
Windson <wiwindson@outlook.com>
2018-03-12 16:53:22 +0800

    clean unused files

--- cherry/bayes.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-cherry.bayes
-~~~~~~~~~~~~
-This module implements the bayes classify.
-:copyright: (c) 2018 by Windson Yang
-:license: MIT License, see LICENSE for more details.
-"""
--- cherry/trainer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-cherry.trainer
-~~~~~~~~~~~~
-This module implements native bayes trainer
-:copyright: (c) 2018 by Windson Yang
-:license: MIT License, see LICENSE for more details.
-"""
