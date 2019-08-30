cee7d2a63965c6c119b0e5a6da8246aae81c321c
Windsooon <wiwindson@outlook.com>
2019-08-30 12:16:25 +0800

    update default config

--- cherry/config.py
+++ cherry/config.py
@@ -9,12 +9,22 @@ Base config of cherry classify
 """
 import os
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
-from .base import tokenizer, stop_words
+from .base import stop_words
 
-PREFIX = 'chinese_classify'
-DEFAULT_VECTORIZER = CountVectorizer(tokenizer=tokenizer, stop_words=stop_words(PREFIX))
-DEFAULT_CLF = MultinomialNB()
-NGRAM = (1, 1)
-MAX_DF = 1.0
-MIN_DF = 1
+def tokenizer(text):
+    '''
+    You can use your own tokenizer function here, by default,
+    this function only work for chinese
+    '''
+    # For English, try:
+    # from nltk.tokenize import word_tokenize
+    # return [t.lower() for t in word_tokenize(text) if len(t) > 1]
+    return [t for t in jieba.cut(text) if len(t) > 1]
+
+
+FILENAME = 'chinese_classify.txt'
+STOPWORDS = 'stop_words_' + FILENAME
+DEFAULT_VECTORIZER = CountVectorizer(tokenizer=tokenizer, stop_words=stop_words(STOPWORDS))
+DEFAULT_CLF = MultinomialNB(alpha=0.1)
