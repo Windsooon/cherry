3fdf6b4dc56a99fd13bceedf1d52739abd1e6928
Windsooon <wiwindson@outlook.com>
2019-09-03 10:31:45 +0800

    add tests for trainer

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -2,10 +2,8 @@ import os
 import unittest
 from unittest import mock
 from sklearn.pipeline import Pipeline
-from cherry.config import DEFAULT_VECTORIZER, DEFAULT_CLF
-from cherry.base import DATA_DIR
 from cherry.trainer import Trainer
-
+from cherry.base import DATA_DIR, load_data
 
 class TrainerTest(unittest.TestCase):
 
@@ -18,17 +16,62 @@ class TrainerTest(unittest.TestCase):
         '''
         TODO: use create special cache files for testing
         '''
-        mock_train.return_value = DEFAULT_VECTORIZER, DEFAULT_CLF
-        t = Trainer(vectorizer=DEFAULT_VECTORIZER, clf=DEFAULT_CLF, x_data=self.x_data, y_data=self.y_data)
-        self.assertTrue(os.path.exists(os.path.join(DATA_DIR, 'trained.pkl')))
-        self.assertTrue(os.path.exists(os.path.join(DATA_DIR, 've.pkl')))
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
+        self.assertTrue(os.path.exists(os.path.join(DATA_DIR, 'harmful/trained.pkl')))
+        self.assertTrue(os.path.exists(os.path.join(DATA_DIR, 'harmful/ve.pkl')))
 
+    @mock.patch('cherry.trainer.load_data')
+    @mock.patch('cherry.trainer.Trainer._write_cache')
+    @mock.patch('cherry.trainer.Trainer.train')
+    def test_init_load_data(self, mock_train, mock_cache, mock_load):
+        mock_load.return_value = self.x_data, self.y_data
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=None, y_data=None)
+        mock_load.assert_called_once_with('harmful')
+
+    @mock.patch('cherry.trainer.load_data')
+    @mock.patch('cherry.trainer.Trainer._write_cache')
+    @mock.patch('cherry.trainer.Trainer.train')
+    def test_init_pass_data(self, mock_train, mock_cache, mock_load):
+        mock_load.return_value = self.x_data, self.y_data
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
+        mock_load.assert_not_called()
+
+    @mock.patch('cherry.trainer.get_vectorizer')
+    @mock.patch('cherry.trainer.Trainer._write_cache')
+    @mock.patch('cherry.trainer.Trainer.train')
+    def test_init_no_vectorizer(self, mock_train, mock_cache, mock_vect):
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
+        mock_vect.assert_called_once_with('harmful', None)
+
+    @mock.patch('cherry.trainer.get_clf')
+    @mock.patch('cherry.trainer.Trainer._write_cache')
+    @mock.patch('cherry.trainer.Trainer.train')
+    def test_init_no_clf(self, mock_train, mock_cache, mock_clf):
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
+        mock_clf.assert_called_once_with('harmful', None)
+
+    @mock.patch('cherry.trainer.Trainer._write_cache')
+    @mock.patch('cherry.trainer.Trainer.train')
+    def test_init_train(self, mock_train, mock_cache):
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
+        mock_train.assert_called_once()
+
+    @mock.patch('cherry.trainer.Trainer._write_cache')
     @mock.patch('sklearn.pipeline.Pipeline.fit')
-    def test_Trainer_init(self, mock_fit):
-        mock_fit.return_value = DEFAULT_VECTORIZER, DEFAULT_CLF
-        x_data = [1,2]
-        y_data = [0,1]
-        t = Trainer(vectorizer=DEFAULT_VECTORIZER, clf=DEFAULT_CLF, x_data=x_data, y_data=y_data)
-        vectorizer, clf = t.train(DEFAULT_VECTORIZER, DEFAULT_CLF, x_data, y_data)
-        self.assertEqual(vectorizer, DEFAULT_VECTORIZER)
-        self.assertEqual(clf, DEFAULT_CLF)
+    def test_train_fit(self, mock_fit, mock_cache):
+        t = Trainer(
+            model='harmful', vectorizer=None, vectorizer_method=None,
+            clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
+        mock_fit.assert_called_once_with(self.x_data, self.y_data)
