eaecf0ab7d3f422114edfa95ee95b2d6685ae7dc
Windsooon <wiwindson@outlook.com>
2019-09-07 10:18:57 +0800

    fix tests

--- tests/test_display.py
+++ tests/test_display.py
@@ -10,30 +10,41 @@ class DisplayTest(unittest.TestCase):
         self.x_data = [1, 2]
         self.y_data = [0, 1]
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.displayer.load_data')
     @mock.patch('cherry.displayer.Display.display_learning_curve')
-    def test_init_load_data(self, mock_learning, mock_load):
+    def test_init_load_data(self, mock_learning, mock_load, mock_stop_words):
+        mock_stop_words.return_value = ['first', 'second']
         mock_load.return_value = self.x_data, self.y_data
         d = Display(model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=None, y_data=None)
         mock_load.assert_called_once_with('harmful')
 
+    @mock.patch('cherry.displayer.load_data')
     @mock.patch('cherry.displayer.get_vectorizer')
     @mock.patch('cherry.displayer.Display.display_learning_curve')
-    def test_init_no_vect(self, mock_learning, mock_vect):
+    def test_init_no_vect(self, mock_learning, mock_vect, mock_load):
+        mock_load.return_value = self.x_data, self.y_data
         d = Display(model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=None, y_data=None)
         mock_vect.assert_called_once_with('harmful', None)
 
+    @mock.patch('cherry.displayer.load_data')
+    @mock.patch('cherry.displayer.get_vectorizer')
     @mock.patch('cherry.displayer.get_clf')
     @mock.patch('cherry.displayer.Display.display_learning_curve')
-    def test_init_no_clf(self, mock_learning, mock_clf):
+    def test_init_no_clf(self, mock_learning, mock_clf, mock_vect, mock_load):
+        mock_load.return_value = self.x_data, self.y_data
         d = Display(model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=None, y_data=None)
         mock_clf.assert_called_once_with('harmful', None)
 
+
+    @mock.patch('cherry.displayer.load_data')
+    @mock.patch('cherry.displayer.get_vectorizer')
     @mock.patch('cherry.displayer.Display.plot_learning_curve')
-    def test_init_no_clf(self, mock_learning):
+    def test_init_no_clf_vect(self, mock_learning, mock_vect, mock_load):
+        mock_load.return_value = self.x_data, self.y_data
         d = Display(model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=None, y_data=None)
         mock_learning.assert_called_once()
--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -10,31 +10,37 @@ class TrainerTest(unittest.TestCase):
         self.x_data = [1, 2]
         self.y_data = [0, 1]
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.trainer.Trainer.train')
-    def test_write_cache(self, mock_train):
+    def test_write_cache(self, mock_train, mock_stop_words):
         '''
         TODO: use create special cache files for testing
         '''
+        mock_stop_words.return_value = ['first', 'second']
         t = Trainer(
             model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
         self.assertTrue(os.path.exists(os.path.join(DATA_DIR, 'harmful/trained.pkl')))
         self.assertTrue(os.path.exists(os.path.join(DATA_DIR, 'harmful/ve.pkl')))
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.trainer.load_data')
     @mock.patch('cherry.trainer.Trainer._write_cache')
     @mock.patch('cherry.trainer.Trainer.train')
-    def test_init_load_data(self, mock_train, mock_cache, mock_load):
+    def test_init_load_data(self, mock_train, mock_cache, mock_load, mock_stop_words):
+        mock_stop_words.return_value = ['first', 'second']
         mock_load.return_value = self.x_data, self.y_data
         t = Trainer(
             model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=None, y_data=None)
         mock_load.assert_called_once_with('harmful')
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.trainer.load_data')
     @mock.patch('cherry.trainer.Trainer._write_cache')
     @mock.patch('cherry.trainer.Trainer.train')
-    def test_init_pass_data(self, mock_train, mock_cache, mock_load):
+    def test_init_pass_data(self, mock_train, mock_cache, mock_load, mock_stop_words):
+        mock_stop_words.return_value = ['first', 'second']
         mock_load.return_value = self.x_data, self.y_data
         t = Trainer(
             model='harmful', vectorizer=None, vectorizer_method=None,
@@ -50,26 +56,32 @@ class TrainerTest(unittest.TestCase):
             clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
         mock_vect.assert_called_once_with('harmful', None)
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.trainer.get_clf')
     @mock.patch('cherry.trainer.Trainer._write_cache')
     @mock.patch('cherry.trainer.Trainer.train')
-    def test_init_no_clf(self, mock_train, mock_cache, mock_clf):
+    def test_init_no_clf(self, mock_train, mock_cache, mock_clf, mock_stop_words):
+        mock_stop_words.return_value = ['first', 'second']
         t = Trainer(
             model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
         mock_clf.assert_called_once_with('harmful', None)
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.trainer.Trainer._write_cache')
     @mock.patch('cherry.trainer.Trainer.train')
-    def test_init_train(self, mock_train, mock_cache):
+    def test_init_train(self, mock_train, mock_cache, mock_stop_words):
+        mock_stop_words.return_value = ['first', 'second']
         t = Trainer(
             model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
         mock_train.assert_called_once()
 
+    @mock.patch('cherry.base.get_stop_words')
     @mock.patch('cherry.trainer.Trainer._write_cache')
     @mock.patch('sklearn.pipeline.Pipeline.fit')
-    def test_train_fit(self, mock_fit, mock_cache):
+    def test_train_fit(self, mock_fit, mock_cache, mock_stop_words):
+        mock_stop_words.return_value = ['first', 'second']
         t = Trainer(
             model='harmful', vectorizer=None, vectorizer_method=None,
             clf=None, clf_method=None, x_data=self.x_data, y_data=self.y_data)
