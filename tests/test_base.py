d8ecdad79ac5e3af6038a260cf67523214402496
Windsooon <wiwindson@outlook.com>
2020-05-17 21:19:34 +0800

    add some tests for base.py

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,8 @@
+import os
+import shutil
 import unittest
 from unittest import mock
+import tempfile
 import cherry
 from cherry.base import *
 
@@ -8,6 +11,7 @@ class BaseTest(unittest.TestCase):
 
     def test_stop_words(self):
         self.assertIn("anyone", get_stop_words())
+        self.assertNotIn("human", get_stop_words())
 
     def test_load_data_not_found(self):
         with self.assertRaises(cherry.exceptions.FilesNotFoundError) as filesNotFoundError:
@@ -15,3 +19,23 @@ class BaseTest(unittest.TestCase):
         self.assertEqual(
             str(filesNotFoundError.exception),
             'harmful is not built in models and not found in dataset folder.')
+
+    @mock.patch('cherry.base.load_files')
+    def test_load_local_data_from_local_without_cache(self, mock_load_files):
+        mock_load_files.return_value = None
+        res = _load_data_from_local('foo', 'foo')
+        self.assertEqual(res, None)
+        mock_load_files.assert_called_once_with('foo', categories=None, encoding=None)
+
+    def test_load_local_data_from_local_with_cache(self):
+        dir_path = os.path.join(DATA_DIR, 'foo')
+        os.mkdir(dir_path)
+        with open(os.path.join(dir_path, 'foo.pkz'),'wb+') as f:
+            f.write(b'bar')
+        res = _load_data_from_local(os.path.join(DATA_DIR, 'foo'), 'foo')
+        self.assertEqual(res['data'], [])
+        shutil.rmtree(dir_path)
+
+    def test_load_data_from_remote_not_build_in(self):
+        pass
+
