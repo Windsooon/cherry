2684696dc4be32f5331a1df1fe73eb883f17617a
Windsooon <wiwindson@outlook.com>
2020-05-17 20:30:00 +0800

    clean up code

--- cherry/exceptions.py
+++ cherry/exceptions.py
@@ -3,9 +3,18 @@
 cherry.exceptions
 ~~~~~~~~~~~~~~~~~~~
 This module contains the set of cheery' exceptions.
-:copyright: (c) 2018-2019 by Windson Yang
+:copyright: (c) 2018-2020 by Windson Yang
 :license: MIT License, see LICENSE for more details.
 """
+
+__all__ = ['NotSupportError',
+           'CacheNotFoundError',
+           'FilesNotFoundError',
+           'DownloadError',
+           'MethodNotFoundError',
+           'TokenNotFoundError',
+           'DataMismatchError',
+           'UnicodeFileEncodeError']
 
 class CherryException(Exception):
     '''Base Exception'''
@@ -15,6 +24,8 @@ class CherryException(Exception):
     def __str__(self):
         return self.error
 
+class NotSupportError(CherryException):
+    '''Not support this feature'''
 
 class CacheNotFoundError(IOError):
     '''Cache files not found'''
